e23ab4b3db7af5de70e537cf448b234dfddce07a
Shirin <shirinetezazian@gmail.com>
2021-12-11 17:13:12 -0700

    Find the intersection of two lists in O(n)

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ function Node(val) {
   return obj;
 }
 
-// Find the intersection of two lists in O(n^2)
+// Find the intersection of two lists in O(n)
 function linkedListIntersection(list1, list2) {
+  const items = {};
   for (let node1 = list1; node1 !== null; node1 = node1.next) {
-    for (let node2 = list2; node2 !== null; node2 = node2.next) {
-      if (node2.value === node1.value) {
-        return node1.value;
-      }
+    items[node1.value] = true;
+  }
+  for (let node2 = list2; node2 !== null; node2 = node2.next) {
+    if (items[node2.value]) {
+      return node2.value;
     }
   }
   return null;
